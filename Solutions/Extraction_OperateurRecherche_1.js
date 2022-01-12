7bb8ca730495b5a6b39bc4dbd10e68ac384e5cae
Person M <michael.person@cognitic.be>
2022-01-12 16:05:16 +0100

    Ajout des deux premières solutions pour les opérateurs de recherche

---
+++ Solutions/Extraction_OperateurRecherche_1.js
@@ -0,0 +1,17 @@
+/*Afficher le NOM des clients dont le COMPTE est strictement négatif*/
+db.Client.find(
+    {
+        'COMPTE':{$lt:0}
+    }
+,
+    {
+        _id:0,
+        'NOM':true
+    }
+)
+
+
+
+
+
+
---
+++ Solutions/Extraction_OperateurRecherche_2.js
@@ -0,0 +1,18 @@
+/*Afficher le NOM et la LOCALITE des clients dont le COMPTE est vide
+*/
+
+db.Client.find(
+    {
+        'COMPTE':{$eq:0}
+    }
+,
+    {
+        'NOM':1,
+        'LOCALITE':1
+    }
+)
+
+
+
+
+
